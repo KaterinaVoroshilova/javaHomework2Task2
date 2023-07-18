70c1c4f45d8676631be88ee83f3b0b3d7fe0c491
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-07-18 14:32:28 +0300

    task2 add

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int startBalance = 100;
+        int deposit = 1100;
+        int bonus = 100;
+        int bonusCash = 0;
+        int endBalance = startBalance + deposit;
+
+        if(deposit > 1000){
+            bonusCash = deposit/bonus;
+            endBalance = endBalance + bonusCash;
+        }
+
+        System.out.println("Количество бонусных рублей: " + bonusCash);
+        System.out.println("Итоговый счет: " + endBalance);
+    }
+}
